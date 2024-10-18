2a7bc5af9ac127d06855856896eafa026753675f
DarkCoder28 <agsheeran@duck.com>
2024-10-17 20:47:11 -0400

    Implemented part of the protocol version structure

--- server/src/matrix_server/server.rs
+++ server/src/matrix_server/server.rs
@@ -1,6 +1,6 @@
 use std::{io, net::SocketAddr, time::Duration};
 
-use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::{TcpListener, TcpStream}, time::sleep};
+use tokio::{io::{AsyncBufReadExt, AsyncWriteExt, BufReader}, net::{TcpListener, TcpStream}, time::sleep};
 
 use crate::{boards::BoardRender, config_manager::ConfigWrapper, state_manager::StateWrapper};
 
@@ -24,10 +24,14 @@ pub async fn run_matrix_server(config: ConfigWrapper, state: StateWrapper) -> io
 
 async fn process_connection(mut socket: TcpStream, address: SocketAddr, config: ConfigWrapper, state: StateWrapper) {
     tracing::info!("New connection from [{}:{}]", address.ip(), address.port());
+    let (reader, mut writer) = socket.split();
     // Setup Session
     {
-        match tokio::time::timeout(Duration::from_secs(5), socket.read_u64()).await {
-            Ok(version) => {
+        let mut reader = BufReader::new(reader);
+        let mut version_buff = String::new();
+        match tokio::time::timeout(Duration::from_secs(5), reader.read_line(&mut version_buff)).await {
+            Ok(_res) => {
+                let _ = version_buff.split_off(version_buff.len()-1);
                 let mut config = config.write().await;
                 if !config.device_configs.contains_key(&address.ip().to_string()) {
                     let default_config = config.device_configs.get("default").unwrap().clone();
@@ -35,7 +39,11 @@ async fn process_connection(mut socket: TcpStream, address: SocketAddr, config: 
                     config.save();
                 }
                 let device_config = config.device_configs.get_mut(&address.ip().to_string()).unwrap();
-                device_config.proto_version = version.unwrap_or(0);
+                if let Ok(version) = version_buff.parse::<u64>() {
+                    device_config.proto_version = version;
+                } else {
+                    device_config.proto_version = 0;
+                }
             },
             Err(_) => {
                 tracing::info!("Connection [{}:{}] did not send version. Falling back to legacy mode.", address.ip(), address.port());
@@ -68,7 +76,7 @@ async fn process_connection(mut socket: TcpStream, address: SocketAddr, config: 
                 current_board_name = local_config.device_configs.get(&address.ip().to_string()).unwrap().boards.get(current_board).unwrap();
             }
             let board = local_config.get_boards().get(current_board_name).expect("Failed to get board (clock)");
-            if socket.write_all(board.render(device_config, config.clone(), state.clone()).await.as_bytes()).await.is_err() {
+            if writer.write_all(board.render(device_config, config.clone(), state.clone()).await.as_bytes()).await.is_err() {
                 tracing::info!("Connection from [{}:{}] closed.", address.ip(), address.port());
                 return;
             }
